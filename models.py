b85c812cd0338a9d4556b1beec62029288b6f2f1
MaksymYurkevych <102907831+MaksymYurkevych@users.noreply.github.com>
2023-05-22 14:57:20 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from mongoengine import connect, Document, StringField, ListField, CASCADE, ReferenceField
 
-connect(host='mongodb+srv://userweb10:567234@pyweb.e9uvnnc.mongodb.net/?retryWrites=true&w=majority')
+connect(host='mongodb+srv://mydb:bumegalol@matz.2ewqgqh.mongodb.net/?retryWrites=true&w=majority')
 
 
 class Author(Document):
